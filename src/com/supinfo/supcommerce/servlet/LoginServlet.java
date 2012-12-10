cdbb5793d9d882dfb3c7e12b9dcb0d30f2ea36f8
Antoine Rouaze <antoine.rouaze@zenika.com>
2012-12-10 15:50:42 +0100

    Add correction n°2.

---
+++ src/com/supinfo/supcommerce/servlet/LoginServlet.java
@@ -0,0 +1,24 @@
+package com.supinfo.supcommerce.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+/**
+ * @author Antoine Rouaze <antoine.rouaze@zenika.com>
+ */
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        HttpSession session = req.getSession(true);
+        session.setAttribute("username", username);
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/listProducts");
+        dispatcher.forward(req, resp);
+    }
+}
